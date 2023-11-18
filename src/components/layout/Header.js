bb99168d955e10631e95710ead6119eb169965ec
nader <esmailjafarpour29@gmail.com>
2023-11-18 13:34:38 +0330

    Creating Header & Footer components and using them in the Layout component and using this component in the layout file in the app

---
+++ src/components/layout/Header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FiLogIn } from "react-icons/fi";
+import styles from "@/layout/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">صفحه ی اصلی</Link>
+          </li>
+          <li>
+            <Link href="/buy-residential">صفحه ی اصلی</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.login}>
+        <Link href="/signin">
+          <FiLogIn />
+          <span>ورود</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
