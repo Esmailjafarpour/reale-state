50d422748173a7f73bbafe6a0ecea347d7a8e5f9
nader <esmailjafarpour29@gmail.com>
2023-11-29 15:52:45 +0330

    create api profile

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,85 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import Types from "mongoose";
+import connectDB from "@/utils/connectDB";
+import Profile from "@/models/Profile";
+import User from "@/models/Profile";
+
+async function POST(req) {
+  try {
+    await connectDB();
+    const body = await req.json();
+    console.log("body profile api", body);
+    const {
+      title,
+      description,
+      location,
+      phone,
+      price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+    } = body;
+
+    const session = await getServerSession(req);
+    console.log("session profile api", session);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "لطفا حساب کاربری خود را ایجاد کنید" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !title ||
+      !description ||
+      !location ||
+      !phone ||
+      !price ||
+      !realState ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفااطلاعات صحیح را وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const newProfile = await Profile.create({
+      title,
+      description,
+      location,
+      phone,
+      price: +price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+      userId: new Types.ObjectId(user._id),
+    });
+    console.log("newProfile", newProfile);
+
+    return NextResponse.json(
+      { message: "آگهی جدید اضافه شد" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("error from profile api");
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
